a3f83a9176877f08e4b4b97fea4f7985acc977e3
Taras Tsalko <you@example.com>
2025-06-15 19:47:54 +0300

    Доработана функция AnalyseFunctions согласно заданию, добавлена функция PrintResultAnalyseFunction для вывода результатов

--- include/analyse.hpp
+++ include/analyse.hpp
@@ -24,24 +24,34 @@
 
 namespace analyser {
 
+using namespace analyser::metric;
+using namespace analyser::file;
+using namespace analyser::function;
+
+namespace vs = std::views;
 namespace rv = std::ranges::views;
 namespace rs = std::ranges;
+
+using MetricsToFuncs = std::vector<std::pair<Function, MetricResults>>;
 
 auto AnalyseFunctions(const std::vector<std::string> &files,
                       const analyser::metric::MetricExtractor &metric_extractor) {
 
-    // TODO: первоночальный вариант с циклами, что бы отладить логику,
-    // а потом переписать с использованием ranges и views
-    using namespace analyser::metric;
-    MetricResults results;
-    using namespace analyser::function;
+    using Functions = std::vector<Function>;
+    MetricsToFuncs results;
+
+    Functions functions;
     FunctionExtractor functionExtractor;
-    auto all_metrics = files | std::views::transform([&](const auto &file) { return functionExtractor.Get(file); }) |
-                       std::views::join |
-                       std::views::transform([&](const auto &func) { return metric_extractor.Get(func); }) |
-                       std::views::join;
-    results.reserve(std::ranges::distance(all_metrics));
-    std::ranges::copy(all_metrics, std::back_inserter(results));
+
+    auto all_functions = files | vs::transform([&functionExtractor](const auto &fullfileName) {
+                             const File file(fullfileName);
+                             return functionExtractor.Get(file);  // returns std::vector<FunctionType>
+                         }) |
+                         vs::join;
+
+    results.reserve(rs::distance(all_functions));
+    rs::transform(all_functions, std::back_inserter(results),
+                  [&metric_extractor](const auto &func) { return std::pair{func, metric_extractor.Get(func)}; });
     return results;
 }
 
@@ -56,6 +66,21 @@ auto SplitByFiles(const auto &analysis) {
 void AccumulateFunctionAnalysis(const auto &analysis,
                                 const analyser::metric_accumulator::MetricsAccumulator &accumulator) {
     // здесь ваш код
+}
+
+void PrintResultAnalyseFunction(const MetricsToFuncs &metricsToFuncs) {
+    auto print_info = [](const auto &info) {
+        const auto &func = info.first;
+        if (func.class_name)
+            std::print("{}[::{}]::{}\n", func.filename, *func.class_name, func.name);
+        else
+            std::print("{}[::None]::{}\n", func.filename, func.name);
+
+        // Inner iteration over metrics using ranges
+        std::ranges::for_each(info.second,
+                              [](const auto &metric) { std::print("\t{}: {}\n", metric.metric_name, metric.value); });
+    };
+    std::ranges::for_each(metricsToFuncs, print_info);
 }
 
 }  // namespace analyser
