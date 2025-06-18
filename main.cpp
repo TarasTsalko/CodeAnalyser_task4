cb7e58e94ef1e533ff6390e1f28f9c1dcf35307b
Taras Tsalko <you@example.com>
2025-06-18 22:42:17 +0300

    Реализована часть сценариев, описанных в main

--- main.cpp
+++ main.cpp
@@ -26,27 +26,46 @@
 #include "metric_accumulator_impl/accumulators.hpp"
 #include "metric_impl/metrics.hpp"
 
+
 int main(int argc, char *argv[]) {
+
     analyser::cmd::ProgramOptions options;
+
     // распарсите входные параметры
+    if( !options.Parse( argc, argv ) )
+        return 0;
+
+    namespace metric_impl      = analyser::metric::metric_impl;
+    namespace accamulator_impl = analyser::metric_accumulator::metric_accumulator_impl;
+    namespace accumulator_interface = analyser::metric_accumulator;
+
+    const std::vector<std::string>& inFiles = options.GetFiles();
 
     // analyser::metric::MetricExtractor metric_extractor;
     // зарегистрируйте метрики в metric_extractor
-
+    analyser::metric::MetricExtractor metric_extractor;
+    metric_extractor.RegisterMetric( std::make_unique<metric_impl::CodeLinesCountMetric>() );
+    metric_extractor.RegisterMetric( std::make_unique<metric_impl::CountParametersMetric>() );
+    metric_extractor.RegisterMetric( std::make_unique<metric_impl::CyclomaticComplexityMetric>() );
+    
     // запустите analyser::AnalyseFunctions
-    // выведете результаты анализа на консоль
-
-    // analyser::metric_accumulator::MetricsAccumulator accumulator;
+    auto metricsToFuncs = analyser::AnalyseFunctions( inFiles, metric_extractor );
+    
     // зарегистрируйте аккумуляторы метрик в accumulator
-
-    // запустите analyser::SplitByFiles
-    // запустите analyser::AccumulateFunctionAnalysis для каждого подмножества результатов метрик
-    // выведете результаты на консоль
-
-    // запустите analyser::SplitByClasses
-    // запустите analyser::AccumulateFunctionAnalysis для каждого подмножества результатов метрик
-    // выведете результаты на консоль
-
+    analyser::metric_accumulator::MetricsAccumulator accumulator;
+    accumulator.RegisterAccumulator( "CodeLinesCountMetric", std::make_unique<accamulator_impl::SumAverageAccumulator>() );
+    accumulator.RegisterAccumulator( "CyclomaticComplexityMetric", std::make_unique<accamulator_impl::SumAverageAccumulator>() );
+    accumulator.RegisterAccumulator( "CountParametersMetric", std::make_unique<accamulator_impl::AverageAccumulator>() );
+    
+    std::println( "========Разбиение по файлам===========" );
+    auto splittedByFiles = analyser::SplitByFiles( metricsToFuncs );
+    analyser::PrintResultAnalyseSplittedByGroup( splittedByFiles, accumulator, "file" );
+   
+    std::println();
+    std::println( "========Разбиение по классам===========" );
+    auto splittedByClasses = analyser::SplitByClasses( metricsToFuncs );
+    analyser::PrintResultAnalyseSplittedByGroup( splittedByClasses, accumulator, "class" );
+   
     // запустите analyser::AccumulateFunctionAnalysis для всех результатов метрик
     // выведете результаты на консоль
 
