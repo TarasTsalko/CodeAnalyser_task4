c3322b72072b211b5878fed512563fc18e5e1e12
Taras Tsalko <you@example.com>
2025-06-20 19:10:46 +0300

    Доработана реализация метрики по количеству строк кода, строка с названием функции так же учитывается

--- src/metric_impl/code_lines_count.cpp
+++ src/metric_impl/code_lines_count.cpp
@@ -25,25 +25,18 @@ namespace analyser::metric::metric_impl {
 MetricResult::ValueType CodeLinesCountMetric::CalculateImpl(const function::Function &f) const {
     const size_t MAX_VALUE = std::numeric_limits<size_t>::max();
     size_t prevPos = MAX_VALUE;
-    bool bodyOfFunctionStarted = false;
-    auto checker = [&prevPos, &bodyOfFunctionStarted](auto &&part) {
+
+    auto checker = [&prevPos](auto &&part) {
         std::string_view sv(&*part.begin(), std::ranges::distance(part));
 
-        // считаем количество строк в функции без заголовка с параметрами
-        if (!bodyOfFunctionStarted) {
-            bodyOfFunctionStarted = sv.contains("body");
-            //  даже если нашли body саму строку с тэгом пропускаем
-            return false;
-        }
-
-        if (!bodyOfFunctionStarted)
-            return false;
-
+        // считаем количество линий в коде функции вместе с заголовкрм функции
         // пропускаем:
         // "comment" - однострочные коментарии
         //  "expression_statement" - за "expression_statement" может идти не только действие, но и "string",
         //  то есть многострочный комментарий
-        if (sv.contains("comment") || sv.contains("expression_statement") || sv.contains("string"))
+        //  body - служебный тэг, его тоже пропускаем
+        if (sv.contains("comment") || sv.contains("expression_statement") || sv.contains("string") ||
+            sv.contains("body"))
             return false;
 
         // получаем индек начала строки без использования циклов
@@ -53,15 +46,16 @@ MetricResult::ValueType CodeLinesCountMetric::CalculateImpl(const function::Func
                             | std::views::join  // Получившиеся чфрагменты (после '[' объединяем строку для take_while )
                             | std::views::take_while([](char c) { return c != ','; });  // берем часть до первой ','
 
-        // преобразовывем строку в число
+        // преобразовывем строку в числоW
         std::string_view start_pos_sv(&*first_number.begin(), std::ranges::distance(first_number));
         const int currentPos = ToInt(start_pos_sv);
+        // запоминаем текущую позицию
         if (prevPos == MAX_VALUE) {
             prevPos = currentPos;
             return true;
         }
 
-        // если строка в скобках не изменилась, то не считаем её повторно
+        // если номер строки в скобках не изменился, то не считаем её повторно (блок дерева продалжается)
         // строки виде a = 5; b = 7; (в AST-дереве отображаются несколькими строками)
         if (prevPos == currentPos)
             return false;
@@ -74,6 +68,5 @@ MetricResult::ValueType CodeLinesCountMetric::CalculateImpl(const function::Func
 }
 
 std::string CodeLinesCountMetric::Name() const { return std::string("CodeLinesCountMetric"); }
-// здесь ваш код
 
 }  // namespace analyser::metric::metric_impl
